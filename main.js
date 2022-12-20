c0ba2cb58fbbe6e5b15442e8bcb3347519c6ad07
markrusselldev <mark@markrussell.dev>
2022-12-19 17:18:33 -0700

    feat: add operate function

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-
-const add = function (a, b) {
-  return a + b;
+/* math functions */
+const add = function (arr) {
+  return arr.reduce((last, next) => last + next, 0);
 };
-
-const subtract = function (a, b) {
-  return a - b;
+// no initial value for subtract
+const subtract = function (arr) {
+  return arr.reduce((last, next) => last - next);
 };
 
 const multiply = function (arr) {
   return arr.reduce((last, next) => last * next, 1);
 };
-
+// no initial value for divide
 const divide = function (arr) {
-  return arr.reduce((last, next) => last / next, 1);
+  return arr.reduce((last, next) => last / next);
 };
+
+/* main functions */
+
+let input = [3, "*", 4, "/", 2];
+
+function operate(input) {
+  const operators = input.filter(e => e === "+" || e === "-" || e === "*" || e === "/");
+  const numbers = input.filter(e => !operators.includes(e));
+  console.log(operators, numbers);
+}
+operate(input);
